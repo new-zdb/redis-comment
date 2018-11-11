59f403fdafab854860e9be7f905eef7340ce5c24
zdb <zy13925zdb@gmail.com>
2018-11-11 15:14:36 +0800

    注释完 intset.c 以及intset.h

---
+++ src/intset.c
@@ -0,0 +1,513 @@
+/*
+ * Copyright (c) 2009-2012, Pieter Noordhuis <pcnoordhuis at gmail dot com>
+ * Copyright (c) 2009-2012, Salvatore Sanfilippo <antirez at gmail dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Redis nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "intset.h"
+#include "zmalloc.h"
+#include "endianconv.h"
+
+/* Note that these encodings are ordered, so:
+ * INTSET_ENC_INT16 < INTSET_ENC_INT32 < INTSET_ENC_INT64. */
+#define INTSET_ENC_INT16 (sizeof(int16_t))
+#define INTSET_ENC_INT32 (sizeof(int32_t))
+#define INTSET_ENC_INT64 (sizeof(int64_t))
+
+/* Return the required encoding for the provided value. */
+// 判断对某个值使用哪种编码
+static uint8_t _intsetValueEncoding(int64_t v) {
+    if (v < INT32_MIN || v > INT32_MAX)
+        return INTSET_ENC_INT64;
+    else if (v < INT16_MIN || v > INT16_MAX)
+        return INTSET_ENC_INT32;
+    else
+        return INTSET_ENC_INT16;
+}
+
+/* Return the value at pos, given an encoding. */
+// 从偏移 pos处使用给定的编码进行编码并返回
+static int64_t _intsetGetEncoded(intset *is, int pos, uint8_t enc) {
+    int64_t v64;
+    int32_t v32;
+    int16_t v16;
+
+    if (enc == INTSET_ENC_INT64) {
+        memcpy(&v64,((int64_t*)is->contents)+pos,sizeof(v64));
+        // 大小端转换，可以忽略
+        memrev64ifbe(&v64);
+        return v64;
+    } else if (enc == INTSET_ENC_INT32) {
+        memcpy(&v32,((int32_t*)is->contents)+pos,sizeof(v32));
+        memrev32ifbe(&v32);
+        return v32;
+    } else {
+        memcpy(&v16,((int16_t*)is->contents)+pos,sizeof(v16));
+        memrev16ifbe(&v16);
+        return v16;
+    }
+}
+
+// 返回底层数组从 pos开始的一个整数
+static int64_t _intsetGet(intset *is, int pos) {
+    return _intsetGetEncoded(is,pos,intrev32ifbe(is->encoding));
+}
+
+// 在指定位置插入一个元素
+static void _intsetSet(intset *is, int pos, int64_t value) {
+    uint32_t encoding = intrev32ifbe(is->encoding);
+
+    // 这里不用担心编码导致内存不足的问题，这个函数是其他函数内部调用的，保证底层数组
+    // 编码已经重新设置，同时内存也已经分配好了
+    if (encoding == INTSET_ENC_INT64) {
+        ((int64_t*)is->contents)[pos] = value;
+        memrev64ifbe(((int64_t*)is->contents)+pos);
+    } else if (encoding == INTSET_ENC_INT32) {
+        ((int32_t*)is->contents)[pos] = value;
+        memrev32ifbe(((int32_t*)is->contents)+pos);
+    } else {
+        ((int16_t*)is->contents)[pos] = value;
+        memrev16ifbe(((int16_t*)is->contents)+pos);
+    }
+}
+
+// 创建一个空的 intset
+intset *intsetNew(void) {
+    intset *is = zmalloc(sizeof(intset));
+    // 默认使用 16位的整数进行编码
+    is->encoding = intrev32ifbe(INTSET_ENC_INT16);
+    is->length = 0;
+    return is;
+}
+
+// 长度调整成指定大小的 len
+static intset *intsetResize(intset *is, uint32_t len) {
+    uint32_t size = len*intrev32ifbe(is->encoding);
+    is = zrealloc(is,sizeof(intset)+size);
+    return is;
+}
+
+/* Search for the position of "value". Return 1 when the value was found and
+ * sets "pos" to the position of the value within the intset. Return 0 when
+ * the value is not present in the intset and sets "pos" to the position
+ * where "value" can be inserted. */
+// 查找 value在 intset中的位置，找到返回1，没找到返回0，找到将位置赋值给 pos指针
+// 一般当做其他函数的子调用过程
+static uint8_t intsetSearch(intset *is, int64_t value, uint32_t *pos) {
+    int min = 0, max = intrev32ifbe(is->length)-1, mid = -1;
+    int64_t cur = -1;
+
+    // 空的 intset肯定没有
+    if (intrev32ifbe(is->length) == 0) {
+        if (pos) *pos = 0;
+        return 0;
+    } else {
+        /* Check for the case where we know we cannot find the value,
+         * but do know the insert position. */
+        // 不能比最后的值大，也不能比第一个值小，不然肯定不存在
+        if (value > _intsetGet(is,intrev32ifbe(is->length)-1)) {
+            if (pos) *pos = intrev32ifbe(is->length);
+            return 0;
+        } else if (value < _intsetGet(is,0)) {
+            if (pos) *pos = 0;
+            return 0;
+        }
+    }
+
+    // 二分查找
+    while(max >= min) {
+        mid = ((unsigned int)min + (unsigned int)max) >> 1;
+        cur = _intsetGet(is,mid);
+        if (value > cur) {
+            min = mid+1;
+        } else if (value < cur) {
+            max = mid-1;
+        } else {
+            break;
+        }
+    }
+
+    // 找的到就返回对应的位置
+    if (value == cur) {
+        if (pos) *pos = mid;
+        return 1;
+        // 找不到就返回应该插入的位置
+    } else {
+        if (pos) *pos = min;
+        return 0;
+    }
+}
+
+/* Upgrades the intset to a larger encoding and inserts the given integer. */
+// 使用更大的编码方式重新编码，同时插入值
+static intset *intsetUpgradeAndAdd(intset *is, int64_t value) {
+    // 记录当前集合的编码方式
+    uint8_t curenc = intrev32ifbe(is->encoding);
+    // 根据插入的值选取合适的编码
+    uint8_t newenc = _intsetValueEncoding(value);
+    int length = intrev32ifbe(is->length);
+    // 这里的 value值保证比原来的最大值要大，或者比原来的最小值要小
+    // 因此，prepend表示是否从前面添加
+    int prepend = value < 0 ? 1 : 0;
+
+    /* First set new encoding and resize */
+    //设置新的编码方式，同时进行扩容，大小为原来的大小加一
+    is->encoding = intrev32ifbe(newenc);
+    is = intsetResize(is,intrev32ifbe(is->length)+1);
+
+    // 重新分配
+    while(length--)
+        _intsetSet(is,length+prepend,_intsetGetEncoded(is,length,curenc));
+
+    // 看在前分配还是在后分配
+    if (prepend)
+        _intsetSet(is,0,value);
+    else
+        _intsetSet(is,intrev32ifbe(is->length),value);
+    is->length = intrev32ifbe(intrev32ifbe(is->length)+1);
+    // 返回重新分配好的整数集合
+    return is;
+}
+
+// 将 from开始到结尾的所有数组内容移动到 to开始的地方，一般当做子调用过程
+static void intsetMoveTail(intset *is, uint32_t from, uint32_t to) {
+    void *src, *dst;
+    uint32_t bytes = intrev32ifbe(is->length)-from;
+    uint32_t encoding = intrev32ifbe(is->encoding);
+
+    if (encoding == INTSET_ENC_INT64) {
+        src = (int64_t*)is->contents+from;
+        dst = (int64_t*)is->contents+to;
+        bytes *= sizeof(int64_t);
+    } else if (encoding == INTSET_ENC_INT32) {
+        src = (int32_t*)is->contents+from;
+        dst = (int32_t*)is->contents+to;
+        bytes *= sizeof(int32_t);
+    } else {
+        src = (int16_t*)is->contents+from;
+        dst = (int16_t*)is->contents+to;
+        bytes *= sizeof(int16_t);
+    }
+    memmove(dst,src,bytes);
+}
+
+// 添加一个元素，success表示成功修改的个数，已经存在的，*success为 0，插入成功的表示设置为 1
+intset *intsetAdd(intset *is, int64_t value, uint8_t *success) {
+    uint8_t valenc = _intsetValueEncoding(value);
+    uint32_t pos;
+    if (success) *success = 1;
+
+    /* Upgrade encoding if necessary. If we need to upgrade, we know that
+     * this value should be either appended (if > 0) or prepended (if < 0),
+     * because it lies outside the range of existing values. */
+    if (valenc > intrev32ifbe(is->encoding)) {
+        // 这条调用总是会成功，所以 *success提前设置为 1
+        return intsetUpgradeAndAdd(is,value);
+    } else {
+        /* Abort if the value is already present in the set.
+         * This call will populate "pos" with the right position to insert
+         * the value when it cannot be found. */
+        // 查看是否存在，不存在的话，会将该插入的位置存在 pos变量中
+        if (intsetSearch(is,value,&pos)) {
+            if (success) *success = 0;
+            return is;
+        }
+
+        // 每次插入元素都会伴随着容量的调整
+        is = intsetResize(is,intrev32ifbe(is->length)+1);
+        if (pos < intrev32ifbe(is->length)) intsetMoveTail(is,pos,pos+1);
+    }
+
+    _intsetSet(is,pos,value);
+    is->length = intrev32ifbe(intrev32ifbe(is->length)+1);
+    return is;
+}
+
+// 删除一个元素，success变量的作用和插入一个变量的作用相同
+intset *intsetRemove(intset *is, int64_t value, int *success) {
+    uint8_t valenc = _intsetValueEncoding(value);
+    uint32_t pos;
+    if (success) *success = 0;
+
+    if (valenc <= intrev32ifbe(is->encoding) && intsetSearch(is,value,&pos)) {
+        uint32_t len = intrev32ifbe(is->length);
+
+        /* We know we can delete */
+        if (success) *success = 1;
+
+        // 被删除元素的后面元素向前挪一个位置
+        if (pos < (len-1)) intsetMoveTail(is,pos+1,pos);
+        // 调整大小
+        is = intsetResize(is,len-1);
+        is->length = intrev32ifbe(len-1);
+    }
+    return is;
+}
+
+// 判断元素是否存在这个整数集合中
+uint8_t intsetFind(intset *is, int64_t value) {
+    uint8_t valenc = _intsetValueEncoding(value);
+    // valenc <= intrev32ifbe(is->encoding)可以快速的过滤掉大小不符合的判断
+    return valenc <= intrev32ifbe(is->encoding) && intsetSearch(is,value,NULL);
+}
+
+// 返回一个随机的数
+int64_t intsetRandom(intset *is) {
+    return _intsetGet(is,rand()%intrev32ifbe(is->length));
+}
+
+// 返回一个再 pos位置的数，将其存入 value变量中，成功的话返回 1，超过下标范围返回 0
+uint8_t intsetGet(intset *is, uint32_t pos, int64_t *value) {
+    if (pos < intrev32ifbe(is->length)) {
+        *value = _intsetGet(is,pos);
+        return 1;
+    }
+    return 0;
+}
+
+// 返回整数集合的长度
+uint32_t intsetLen(const intset *is) {
+    return intrev32ifbe(is->length);
+}
+
+// 范湖整数集合字节的大小，大小为数组长度 * 数组编码，再加上结构体中定义的变量大小
+size_t intsetBlobLen(intset *is) {
+    return sizeof(intset)+intrev32ifbe(is->length)*intrev32ifbe(is->encoding);
+}
+
+#ifdef REDIS_TEST
+#include <sys/time.h>
+#include <time.h>
+
+#if 0
+static void intsetRepr(intset *is) {
+    for (uint32_t i = 0; i < intrev32ifbe(is->length); i++) {
+        printf("%lld\n", (uint64_t)_intsetGet(is,i));
+    }
+    printf("\n");
+}
+
+static void error(char *err) {
+    printf("%s\n", err);
+    exit(1);
+}
+#endif
+
+static void ok(void) {
+    printf("OK\n");
+}
+
+static long long usec(void) {
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return (((long long)tv.tv_sec)*1000000)+tv.tv_usec;
+}
+
+#define assert(_e) ((_e)?(void)0:(_assert(#_e,__FILE__,__LINE__),exit(1)))
+static void _assert(char *estr, char *file, int line) {
+    printf("\n\n=== ASSERTION FAILED ===\n");
+    printf("==> %s:%d '%s' is not true\n",file,line,estr);
+}
+
+static intset *createSet(int bits, int size) {
+    uint64_t mask = (1<<bits)-1;
+    uint64_t value;
+    intset *is = intsetNew();
+
+    for (int i = 0; i < size; i++) {
+        if (bits > 32) {
+            value = (rand()*rand()) & mask;
+        } else {
+            value = rand() & mask;
+        }
+        is = intsetAdd(is,value,NULL);
+    }
+    return is;
+}
+
+static void checkConsistency(intset *is) {
+    for (uint32_t i = 0; i < (intrev32ifbe(is->length)-1); i++) {
+        uint32_t encoding = intrev32ifbe(is->encoding);
+
+        if (encoding == INTSET_ENC_INT16) {
+            int16_t *i16 = (int16_t*)is->contents;
+            assert(i16[i] < i16[i+1]);
+        } else if (encoding == INTSET_ENC_INT32) {
+            int32_t *i32 = (int32_t*)is->contents;
+            assert(i32[i] < i32[i+1]);
+        } else {
+            int64_t *i64 = (int64_t*)is->contents;
+            assert(i64[i] < i64[i+1]);
+        }
+    }
+}
+
+#define UNUSED(x) (void)(x)
+int intsetTest(int argc, char **argv) {
+    uint8_t success;
+    int i;
+    intset *is;
+    srand(time(NULL));
+
+    UNUSED(argc);
+    UNUSED(argv);
+
+    printf("Value encodings: "); {
+        assert(_intsetValueEncoding(-32768) == INTSET_ENC_INT16);
+        assert(_intsetValueEncoding(+32767) == INTSET_ENC_INT16);
+        assert(_intsetValueEncoding(-32769) == INTSET_ENC_INT32);
+        assert(_intsetValueEncoding(+32768) == INTSET_ENC_INT32);
+        assert(_intsetValueEncoding(-2147483648) == INTSET_ENC_INT32);
+        assert(_intsetValueEncoding(+2147483647) == INTSET_ENC_INT32);
+        assert(_intsetValueEncoding(-2147483649) == INTSET_ENC_INT64);
+        assert(_intsetValueEncoding(+2147483648) == INTSET_ENC_INT64);
+        assert(_intsetValueEncoding(-9223372036854775808ull) ==
+                    INTSET_ENC_INT64);
+        assert(_intsetValueEncoding(+9223372036854775807ull) ==
+                    INTSET_ENC_INT64);
+        ok();
+    }
+
+    printf("Basic adding: "); {
+        is = intsetNew();
+        is = intsetAdd(is,5,&success); assert(success);
+        is = intsetAdd(is,6,&success); assert(success);
+        is = intsetAdd(is,4,&success); assert(success);
+        is = intsetAdd(is,4,&success); assert(!success);
+        ok();
+    }
+
+    printf("Large number of random adds: "); {
+        uint32_t inserts = 0;
+        is = intsetNew();
+        for (i = 0; i < 1024; i++) {
+            is = intsetAdd(is,rand()%0x800,&success);
+            if (success) inserts++;
+        }
+        assert(intrev32ifbe(is->length) == inserts);
+        checkConsistency(is);
+        ok();
+    }
+
+    printf("Upgrade from int16 to int32: "); {
+        is = intsetNew();
+        is = intsetAdd(is,32,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT16);
+        is = intsetAdd(is,65535,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT32);
+        assert(intsetFind(is,32));
+        assert(intsetFind(is,65535));
+        checkConsistency(is);
+
+        is = intsetNew();
+        is = intsetAdd(is,32,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT16);
+        is = intsetAdd(is,-65535,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT32);
+        assert(intsetFind(is,32));
+        assert(intsetFind(is,-65535));
+        checkConsistency(is);
+        ok();
+    }
+
+    printf("Upgrade from int16 to int64: "); {
+        is = intsetNew();
+        is = intsetAdd(is,32,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT16);
+        is = intsetAdd(is,4294967295,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT64);
+        assert(intsetFind(is,32));
+        assert(intsetFind(is,4294967295));
+        checkConsistency(is);
+
+        is = intsetNew();
+        is = intsetAdd(is,32,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT16);
+        is = intsetAdd(is,-4294967295,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT64);
+        assert(intsetFind(is,32));
+        assert(intsetFind(is,-4294967295));
+        checkConsistency(is);
+        ok();
+    }
+
+    printf("Upgrade from int32 to int64: "); {
+        is = intsetNew();
+        is = intsetAdd(is,65535,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT32);
+        is = intsetAdd(is,4294967295,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT64);
+        assert(intsetFind(is,65535));
+        assert(intsetFind(is,4294967295));
+        checkConsistency(is);
+
+        is = intsetNew();
+        is = intsetAdd(is,65535,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT32);
+        is = intsetAdd(is,-4294967295,NULL);
+        assert(intrev32ifbe(is->encoding) == INTSET_ENC_INT64);
+        assert(intsetFind(is,65535));
+        assert(intsetFind(is,-4294967295));
+        checkConsistency(is);
+        ok();
+    }
+
+    printf("Stress lookups: "); {
+        long num = 100000, size = 10000;
+        int i, bits = 20;
+        long long start;
+        is = createSet(bits,size);
+        checkConsistency(is);
+
+        start = usec();
+        for (i = 0; i < num; i++) intsetSearch(is,rand() % ((1<<bits)-1),NULL);
+        printf("%ld lookups, %ld element set, %lldusec\n",
+               num,size,usec()-start);
+    }
+
+    printf("Stress add+delete: "); {
+        int i, v1, v2;
+        is = intsetNew();
+        for (i = 0; i < 0xffff; i++) {
+            v1 = rand() % 0xfff;
+            is = intsetAdd(is,v1,NULL);
+            assert(intsetFind(is,v1));
+
+            v2 = rand() % 0xfff;
+            is = intsetRemove(is,v2,NULL);
+            assert(!intsetFind(is,v2));
+        }
+        checkConsistency(is);
+        ok();
+    }
+
+    return 0;
+}
+#endif
